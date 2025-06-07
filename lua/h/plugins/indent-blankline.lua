056740e80a2ae5afcc10f465b6b4ed3638738416
kiviuk <kivik@example.com>
2025-06-07 17:43:50 +0200

    lazy loading optimization 2

--- lua/h/plugins/indent-blankline.lua
+++ lua/h/plugins/indent-blankline.lua
@@ -1,6 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
+  event = "VeryLazy",
   main = "ibl",
   opts = {
     indent = { char = "┊" },
--- lua/h/plugins/linting.lua
+++ lua/h/plugins/linting.lua
@@ -1,6 +1,6 @@
 return {
   "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+  event = "VeryLazy",
   config = function()
     local lint = require("lint")
 
